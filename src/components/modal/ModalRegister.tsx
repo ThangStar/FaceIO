bf7cdb02b904ae4780949f8d1745a0370207404e
Nông Văn Thắng <52882277+ThangStar@users.noreply.github.com>
2024-09-09 08:49:17 +0700

    sound noti

--- src/components/modal/ModalRegister.tsx
+++ src/components/modal/ModalRegister.tsx
@@ -30,7 +30,7 @@ function ModalRegister() {
       data as loginDto
     ))
     const isLoginFailed = res?.meta?.rejectedWithValue
-    !isLoginFailed && (window.location.href = `/FaceIO/home`)
+    !isLoginFailed && (window.location.href = `/home`)
   }
   const {
     register, handleSubmit: handleResgisterSubmit,
@@ -55,7 +55,7 @@ function ModalRegister() {
         localStorage.setItem("idToken", JSON.stringify(credential.idToken));
         const user = result.user;
         dispatch(authActions.setProfileToDb())
-        window.location.href = `/FaceIO/home`
+        window.location.href = `/home`
       }).catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }: any) => {
                 console.log('relogin success!');
                 toast(`Chào ${value.user.displayName}!`);
                 setIsLogined(true);
-                pathname == '/' && (window.location.href = `/FaceIO/home`)
+                pathname == '/' && (window.location.href = `/home`)
             })
             .catch((error) => {
                 // Handle Errors here.
