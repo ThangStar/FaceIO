66844bd2f3b915628d7c9c93e4da99087f62203c
Nông Văn Thắng <52882277+ThangStar@users.noreply.github.com>
2024-09-13 09:04:48 +0700

    redirect fix

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
       },
     ],
   },
-  basePath: process.env.NEXT_PUBLIC_NODE_ENV != "PRODUCTION" ?  null : '/FaceIO',
+  basePath: (process.env.NEXT_PUBLIC_NODE_ENV || "PRODUCTION") != "PRODUCTION" ?  null : '/FaceIO',
 };
 
 export default nextConfig;
